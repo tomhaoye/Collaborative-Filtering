9f662db55787e9b1717fb91f50d82174e5d56038
邱奕玮 <tomhaoye@163.com>
2016-05-06 03:35:59 +0800

    cf

--- cf.php
+++ cf.php
@@ -9,12 +9,17 @@ while ($row = mysql_fetch_row($result)) {
 }
 
 //查找Emma没有接触的商品
+//用户id－1＝下标
+$user_index = 2;
 for ($i = 2; $i < 8; $i++) {
-    if (empty($table[5][$i])) {
+    if (empty($table[$user_index][$i])) {
         $never[] = $i;
+    }else{
+        $never = [5,6,7];
     }
 }
 print_r($never);
+
 
 /**
  * 根据余弦公式, 夹角余弦 = 向量点积/ (向量长度的叉积) = ( x1x2 + y1y2 + z1z2) / ( 跟号(x1平方+y1平方+z1平方 ) x 跟号(x2平方+y2平方+z2平方 ) )
@@ -22,18 +27,18 @@ print_r($never);
 $cos = [];
 $denominator1 = 0;
 for ($i = 2; $i < 8; $i++) {
-    if (!empty($table[5][$i])) {
-        $denominator1 += $table[5][$i] * $table[5][$i];
+    if (!empty($table[$user_index][$i])) {
+        $denominator1 += $table[$user_index][$i] * $table[$user_index][$i];
     }
 }
 print_r($table);
 
-for ($i = 0; $i < 5; $i++) {
+for ($i = 0; $i < 6; $i++) {
     $numerator = 0;
     $denominator2 = 0;
-    for ($j = 2; $j < 8; $j++) {
-        if (!empty($table[5][$j]) && !empty($table[$i][$j])) {
-            $numerator += $table[5][$j] * $table[$i][$j];
+    for ($j = 2; $j < 5; $j++) {
+        if (!empty($table[$user_index][$j]) && !empty($table[$i][$j])) {
+            $numerator += $table[$user_index][$j] * $table[$i][$j];
             $denominator2 += $table[$i][$j] * $table[$i][$j];
         }
     }
@@ -49,9 +54,10 @@ print_r($back);
 
 //确定邻居
 $neighbour_group = [];
-$index = count($back) - 1;
+//从非自己开始
+$index = count($back) - 1 - 1;
 for ($i = $index; $i > $index - 3; $i--) {
-    for ($j = 0; $j < 5; $j++) {
+    for ($j = 0; $j < 6; $j++) {
         if ($back[$i] == $cos[$j]) {
             $neighbour_group[$j][] = $cos[$j];
             //没有接触的项
@@ -68,9 +74,12 @@ for ($i = 1; $i < count($never) + 1; $i++) {
     $pre_numerator = 0;
     $pre_denominator = 0;
     foreach ($neighbour_group as $item) {
-        $pre_numerator += $item[0] * $item[$i];
-        $pre_denominator += $item[0];
+        if(!empty($item[$i])) {
+            $pre_numerator += $item[0] * $item[$i];
+            $pre_denominator += $item[0];
+        }
     }
     $pre_score[] = $pre_numerator / ($pre_denominator);
 }
 print_r($pre_score);
+
