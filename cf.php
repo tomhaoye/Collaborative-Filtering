dad441b1fa71b51211ebea03a4b4a33139b5914a
邱奕玮 <tomhaoye@163.com>
2016-05-07 11:41:34 +0800

    import movieslens 100k data

--- cf.php
+++ cf.php
@@ -9,13 +9,18 @@ while ($row = mysql_fetch_row($result)) {
 }
 print_r($table);
 
-//查找没有接触的商品
+//用户数量
+$usr_count = count($table);
+//矩阵列数
+$col_num = mysql_num_fields($result);
+//项目开始列
+$start_col = 2;
 //用户id－1＝下标
 $user_index = 4;
 //预测设置
 $predict = true;
-
-for ($i = 2; $i < 8; $i++) {
+//查找没有接触的商品
+for ($i = $start_col; $i < $col_num; $i++) {
     if (empty($table[$user_index][$i])) {
         $never[] = $i;
     }
@@ -31,11 +36,11 @@ print_r($never);
  * 根据余弦公式, 夹角余弦 = 向量点积/ (向量长度的叉积) = ( x1x2 + y1y2 + z1z2) / ( 跟号(x1平方+y1平方+z1平方 ) x 跟号(x2平方+y2平方+z2平方 ) )
  */
 $cos = [];
-for ($i = 0; $i < 6; $i++) {
+for ($i = 0; $i < $usr_count; $i++) {
     $numerator = 0;
     $denominator1 = 0;
     $denominator2 = 0;
-    for ($j = 2; $j < 8; $j++) {
+    for ($j = $start_col; $j < $col_num; $j++) {
         if (!empty($table[$user_index][$j]) && !empty($table[$i][$j])) {
             $numerator += $table[$user_index][$j] * $table[$i][$j];
             $denominator1 += $table[$user_index][$j] * $table[$user_index][$j];
@@ -59,7 +64,7 @@ $neighbour_group = [];
 //从自己开始找到最相近的三人
 $index = count($back) - 1;
 for ($i = $index; $i > $index - 4; $i--) {
-    for ($j = 0; $j < 6; $j++) {
+    for ($j = 0; $j < $usr_count; $j++) {
         if ($back[$i] === $cos[$j]) {
             $neighbour_group[$j][] = $cos[$j];
             //没有接触的项
---
+++ movies_import.php
@@ -0,0 +1,42 @@
+<?php
+include_once('db_connect.php');
+
+$users = file('dataDir/u.user');
+$items = file('dataDir/u.item');
+$data = file('dataDir/u.data');
+
+$user_sql1 = 'insert ml_users(id,age,gender,occupation) values ';
+foreach ($users as $item) {
+    $item = explode('|', $item);
+    $user_id = $item[0];
+    $user_age = $item[1];
+    $user_gender = $item[2];
+    $user_occupation = $item[3];
+    $u_sql[] = '(' . $user_id . ',' . $user_age . ',' . "'" . $user_gender . "'" . ',' . "'" . $user_occupation . "'" . ')';
+}
+$user_sql2 = implode(',', $u_sql);
+$user_sql = $user_sql1 . $user_sql2;
+mysql_query($user_sql);
+
+
+$item_sql1 = 'insert ml_items(id,title) values ';
+foreach ($items as $item) {
+    $item = explode('|', $item);
+    $item_id = $item[0];
+    $item_title = $item[1];
+    $i_sql = '(' . $item_id . ',' . "'" . $item_title . "'" . ')';
+    $item_sql = $item_sql1 . $i_sql;
+    mysql_query($item_sql);
+}
+
+$data_sql1 = 'insert ml_data(user_id,item_id,rating) values ';
+foreach ($data as $item) {
+    $item = explode('	', $item);
+    $data_user_id = $item[0];
+    $data_item_id = $item[1];
+    $data_rating = $item[2];
+    $d_sql = '(' . $data_user_id . ',' . $data_item_id . ',' . $data_rating . ')';
+    $data_sql = $data_sql1 . $d_sql;
+    mysql_query($data_sql);
+}
+
