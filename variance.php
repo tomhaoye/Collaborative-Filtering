39ea0425e8e410aa3c12c67900268d624cf81117
邱奕玮 <tomhaoye@163.com>
2016-05-25 02:26:27 +0800

    use sql

--- variance.php
+++ variance.php
@@ -1,30 +1,17 @@
 <?php
 include_once('db_connect.php');
 
-$u_sql = 'select count(0) from ml_users';
-$u_r = mysql_query($u_sql);
-$u_c = mysql_fetch_row($u_r)[0];
+$avg_sql = 'select AVG(rating),user_id from ml_data group by user_id';
+$var_sql = 'select VARIANCE(rating),user_id from ml_data group by user_id';
+$rs1 = mysql_query($avg_sql);
+$rs2 = mysql_query($var_sql);
+while($row  = mysql_fetch_row($rs1)){
+    $avg[] = $row[0];
+};
+while($row  = mysql_fetch_row($rs2)){
+    $var[] = $row[0];
+};
 
-for ($i = 1; $i <= $u_c; $i++) {
-    $rating_arr = [];
-    $sql = 'select * from ml_data where user_id = ' . $i;
-    $all_rating = 0;
-    $res = mysql_query($sql);
-    while ($row = mysql_fetch_row($res)) {
-        if ($row[3]) {
-            $all_rating += $row[3];
-            $rating_arr[] = $row[3];
-        }
-    };
-    $num_rows = mysql_num_rows($res);
-    //平均评分
-    $average_rating[] = ($all_rating / $num_rows);
-
-    //方差计算
-    $variance = 0;
-    foreach ($rating_arr as $item) {
-        $variance += pow(($item - $average_rating[$i-1]), 2) / $num_rows;
-    }
-}
-
+print_r($avg);
+print_r($var);
 
