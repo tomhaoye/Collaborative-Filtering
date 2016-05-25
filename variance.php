fe9fcacbd88558e4556cebf648a7f57c3e6f84bd
邱奕玮 <tomhaoye@163.com>
2016-05-25 13:06:12 +0800

    familiar avg & variance

--- variance.php
+++ variance.php
@@ -1,17 +1,44 @@
 <?php
 include_once('db_connect.php');
 
-$avg_sql = 'select AVG(rating),user_id from ml_data group by user_id';
-$var_sql = 'select VARIANCE(rating),user_id from ml_data group by user_id';
+$user_id = 10;
+$per_den = 15;
+
+$avg_sql = 'select AVG(rating),user_id from ml_data group by user_id ORDER BY AVG(rating)';
+$var_sql = 'select VARIANCE(rating),user_id from ml_data group by user_id order by VARIANCE(rating)';
 $rs1 = mysql_query($avg_sql);
 $rs2 = mysql_query($var_sql);
-while($row  = mysql_fetch_row($rs1)){
-    $avg[] = $row[0];
+while ($row = mysql_fetch_row($rs1)) {
+    $avg[$row[1]] = $row[0];
 };
-while($row  = mysql_fetch_row($rs2)){
-    $var[] = $row[0];
+while ($row = mysql_fetch_row($rs2)) {
+    $var[$row[1]] = $row[0];
 };
 
-print_r($avg);
-print_r($var);
+function getNB()
+{
+    $nb1 = $nb2 = [];
+    global $avg, $var, $user_id, $per_den;
+    foreach ($avg as $key => $item) {
+        if (abs($item - $avg[$user_id]) < ($avg[$user_id] / $per_den)) {
+            $nb1[$key] = $item;
+        }
+    }
+    foreach ($var as $key => $item) {
+        if (abs($item - $var[$user_id]) < ($var[$user_id] / $per_den)) {
+            $nb2[$key] = $item;
+        }
+    }
+    $arr = array_intersect_key($nb1, $nb2);
+    if (count($arr) < 10 && $per_den) {
+        --$per_den;
+        getNB();
+    } else {
+        print_r($arr);
+    }
+}
+
+getNB();
+
+
 
